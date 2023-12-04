f3b227b78a06d6570360cc9e8f468f046abc8b49
Sam <romeosamzy@gmail.com>
2023-12-04 09:44:41 +0800

    Added language change for sidebar

--- resources/views/guest/layouts/footer.blade.php
+++ resources/views/guest/layouts/footer.blade.php
@@ -1,189 +1,202 @@
 <footer class="page-footer">
-    <div class="page-footer__top">
-      <div class="uk-container">
-        <div class="uk-grid uk-grid-small uk-child-width-1-4@l uk-child-width-1-2@s" data-uk-grid>
-          <div>
-            <div class="uk-margin-medium">
-              <div class="logo"><a class="logo__link" href="index.html">
-                  <div class="logo__icon"><img src="{{asset('frontend/img/logo.png')}}" width="35%" alt="Kuchai Motor Sdn Bhd"></div>
-                  <div class="logo__text">Kuchai Motor Sdn Bhd</div>
-                </a></div>
-            </div>
-            <div class="uk-margin-medium">
-              <p>Dolore magna aliq quis nostrud ulamco laboris nisi ut aliquip exa comads conseq asuis aute irure dolor reprehenderit.</p>
-            </div>
-            <div class="uk-margin-medium">
-              <div class="support"><a class="support__link" href="tel:0379810331">
-                  <div class="support__icon"><i class="fas fa-headset"></i></div>
-                  <div class="support__desc">
-                    <div class="support__label">Talk with our support</div>
-                    <div class="support__phone">03-7981 0331</div>
-                  </div>
-                </a></div>
-            </div>
-            <div class="uk-margin-medium">
-              <div class="social">
-                <ul class="social-list">
-                  <li class="social-list__item"><a class="social-list__link" href="https://www.facebook.com/p/Kuchai-Motor-100063841241853/"><i class="fab fa-facebook-f"></i></a></li>
-                  <li class="social-list__item"><a class="social-list__link" href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Fkuchaimotor_official%3Ffbclid%3DIwAR0KQymkBphCaSI4nE69rFRgL0I31OKduTXh5e01viBBSaSWLB0Aoe7oosI&h=AT2AWyTF3YpehIrM9jkkqjnZinVu8f4SzV3EaHHj49nZMTIpGJloA8CprhGXawnY1_UOX2Rt0lHsAGmUnW2MuNYB3kWf5EyCc-ZOixgG-YxSWYg_JnA-8V_aiT2OKq1OSf9wrMvBxiS8BlqtiV_j"><i class="fab fa-instagram"></i></a></li>
-                  <li class="social-list__item"><a class="social-list__link" href="https://www.tiktok.com/@kuchaimotor_official?fbclid=IwAR2mOT5vc7iKzlJnBXZ7SWyVdzbYPLo1mBGq66iUMEm_AcL-rDV8WIZfnI4"><i class="fab fa-tiktok"></i></a></li>
-                </ul>
-              </div>
-            </div>
+  <div class="page-footer__top">
+    <div class="uk-container">
+      <div class="uk-grid uk-grid-small uk-child-width-1-4@l uk-child-width-1-2@s" data-uk-grid>
+        <div>
+          <div class="uk-margin-medium">
+            <div class="logo"><a class="logo__link" href="index.html">
+                <div class="logo__icon"><img src="{{asset('frontend/img/logo.png')}}" width="35%"
+                    alt="Kuchai Motor Sdn Bhd"></div>
+                <div class="logo__text">Kuchai Motor Sdn Bhd</div>
+              </a></div>
           </div>
-          <div>
-            <div class="page-footer__column page-footer__menu">
-              <div class="uk-h4">About Kuchai Motor</div>
-              <ul class="list-decor">
-                <li> <a href="{{route('home')}}">Home</a></li>
-                <li> <a href="{{route('new.motors')}}">New Motorcycles</a></li>
-                <li> <a href="{{route('used.motors')}}">Used Motorcycles</a></li>
-                <li> <a href="">Accessories</a></li>
-                <li> <a href="{{route('contact')}}">Contact Us</a></li>
-              </ul>
-            </div>
+          <div class="uk-margin-medium">
+            <p>Dolore magna aliq quis nostrud ulamco laboris nisi ut aliquip exa comads conseq asuis aute irure dolor
+              reprehenderit.</p>
           </div>
-          {{-- <div>
-            <div class="page-footer__column page-footer__news">
-              <div class="uk-h4">Latest News</div>
-              <ul class="latest-news-list">
-                <li>
-                  <div class="latest-news-item">
-                    <div class="latest-news-item__thumb"><img src="assets/img/news-thumb-1.jpg" alt="news-thumb"></div>
-                    <div class="latest-news-item__info"><a class="latest-news-item__title" href="page-blog-article.html">We Allow Each Rider To Customize Their Rides.</a>
-                      <div class="latest-news-item__date">August 25, 2021</div>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="latest-news-item">
-                    <div class="latest-news-item__thumb"><img src="assets/img/news-thumb-2.jpg" alt="news-thumb"></div>
-                    <div class="latest-news-item__info"><a class="latest-news-item__title" href="page-blog-article.html">Motorcycles Which Feels Just Right Like Ease.</a>
-                      <div class="latest-news-item__date">August 25, 2021</div>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="latest-news-item">
-                    <div class="latest-news-item__thumb"><img src="assets/img/news-thumb-3.jpg" alt="news-thumb"></div>
-                    <div class="latest-news-item__info"><a class="latest-news-item__title" href="page-blog-article.html">Gaze Upon Unbelievably Exciting To Futuristic.</a>
-                      <div class="latest-news-item__date">August 25, 2021</div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div> --}}
-          <div>
-            <div class="page-footer__column page-footer__info">
-              <div class="uk-h4">Contact Us</div>
-              <ul class="info-list">
-
-
-                <li>
-                    <div class="info-list-item">
-                      <div class="info-list-item__title">Call us on </div>
-                      <div class="info-list-item__value">03-7981 0331</div>
-                    </div>
-                  </li>
-
-
-                <li>
-                  <div class="info-list-item">
-                    <div class="info-list-item__title">Operation Hours</div>
-                    <div class="info-list-item__value">Monday - Saturday: <br> 09:00 am to 07:00 pm</div>
-                  </div>
-                </li>
-
-                <li>
-                    <div class="info-list-item">
-                      <div class="info-list-item__title">Reach us by email</div>
-                      <div class="info-list-item__value">kuchaimotor@hotmail.my</div>
-                    </div>
-                  </li>
-
-
-                  <li>
-                    <div class="info-list-item">
-                      <div class="info-list-item__title">Showroom Address </div>
-                      <div class="info-list-item__value">kuchaimotor@hotmail.my</div>
-                    </div>
-                  </li>
-
-
-
-
-                {{-- <li>
-                  <div class="info-list-item">
-                    <div class="info-list-item__title">Parts Shop Hours</div>
-                    <div class="info-list-item__value">Monday - Friday: <br> 09:00 am to 06:00 pm</div>
-                  </div>
-                </li> --}}
+          <div class="uk-margin-medium">
+            <div class="support"><a class="support__link" href="tel:0379810331">
+                <div class="support__icon"><i class="fas fa-headset"></i></div>
+                <div class="support__desc">
+                  <div class="support__label">Talk with our support</div>
+                  <div class="support__phone">03-7981 0331</div>
+                </div>
+              </a></div>
+          </div>
+          <div class="uk-margin-medium">
+            <div class="social">
+              <ul class="social-list">
+                <li class="social-list__item"><a class="social-list__link"
+                    href="https://www.facebook.com/p/Kuchai-Motor-100063841241853/"><i
+                      class="fab fa-facebook-f"></i></a></li>
+                <li class="social-list__item"><a class="social-list__link"
+                    href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Fkuchaimotor_official%3Ffbclid%3DIwAR0KQymkBphCaSI4nE69rFRgL0I31OKduTXh5e01viBBSaSWLB0Aoe7oosI&h=AT2AWyTF3YpehIrM9jkkqjnZinVu8f4SzV3EaHHj49nZMTIpGJloA8CprhGXawnY1_UOX2Rt0lHsAGmUnW2MuNYB3kWf5EyCc-ZOixgG-YxSWYg_JnA-8V_aiT2OKq1OSf9wrMvBxiS8BlqtiV_j"><i
+                      class="fab fa-instagram"></i></a></li>
+                <li class="social-list__item"><a class="social-list__link"
+                    href="https://www.tiktok.com/@kuchaimotor_official?fbclid=IwAR2mOT5vc7iKzlJnBXZ7SWyVdzbYPLo1mBGq66iUMEm_AcL-rDV8WIZfnI4"><i
+                      class="fab fa-tiktok"></i></a></li>
               </ul>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="page-footer__bottom">
-      <div class="uk-container">
-        <div class="uk-text-center">Kuchai Motor Sdn Bhd . All rights reserved.</div>
-      </div>
-    </div>
-
-    <div id="offcanvas" data-uk-offcanvas="flip: true; overlay: true; mode: push">
-      <div class="uk-offcanvas-bar"><button class="uk-offcanvas-close" type="button" data-uk-close=""></button>
-        <div class="uk-margin">
-          <div class="logo"><a class="logo__link" href="index.html">
-              <div class="logo__icon">
-                <img src="{{asset('frontend/img/logo.png')}}" width="25%" alt="Kuchai Motor Sdn Bhd">
-              </div>
-              <div class="logo__text">Kuchai Motor</div>
-            </a></div>
+        <div>
+          <div class="page-footer__column page-footer__menu">
+            <div class="uk-h4">About Kuchai Motor</div>
+            <ul class="list-decor">
+              <li> <a href="{{route('home')}}">Home</a></li>
+              <li> <a href="{{route('new.motors')}}">New Motorcycles</a></li>
+              <li> <a href="{{route('used.motors')}}">Used Motorcycles</a></li>
+              <li> <a href="">Accessories</a></li>
+              <li> <a href="{{route('contact')}}">Contact Us</a></li>
+            </ul>
+          </div>
         </div>
-        <div class="uk-margin">
-
-
-          <ul class="uk-nav uk-nav-default uk-nav-parent-icon" data-uk-nav>
-
-            <li><a href="{{route('home')}}">Home</a></li>
-
-            <li class="uk-parent"><a href="#">Motocycles</a>
-              <ul class="uk-nav-sub">
-                <li><a href="{{route('new.motors')}}">New Motorcycles</a></li>
-                <li><a href="{{route('used.motors')}}">Used Motorcycles</a></li>
-
-              </ul>
-            </li>
-
-            <li><a href="">Accessories</a></li>
-
-            <li><a href="{{route('used.motors')}}">Contact Us</a></li>
-
-
-            <li class="uk-parent"><a href="#">Languages</a>
-                <ul class="uk-nav-sub">
-                  <li><a href="#">English </a></li>
-                  <li><a href="#">Bahasa Melayu</a></li>
-
-                </ul>
+        {{-- <div>
+          <div class="page-footer__column page-footer__news">
+            <div class="uk-h4">Latest News</div>
+            <ul class="latest-news-list">
+              <li>
+                <div class="latest-news-item">
+                  <div class="latest-news-item__thumb"><img src="assets/img/news-thumb-1.jpg" alt="news-thumb"></div>
+                  <div class="latest-news-item__info"><a class="latest-news-item__title"
+                      href="page-blog-article.html">We Allow Each Rider To Customize Their Rides.</a>
+                    <div class="latest-news-item__date">August 25, 2021</div>
+                  </div>
+                </div>
               </li>
-          </ul>
-        </div>
-        <div class="uk-margin-auto-top">
-          <div class="support"><a class="support__link" href="tel:0379810331">
-              <div class="support__icon"><i class="fas fa-headset"></i></div>
-              <div class="support__desc">
-                <div class="support__label">Talk with our support</div>
-                <div class="support__phone">03-7981 0331</div>
-              </div>
-            </a></div>
+              <li>
+                <div class="latest-news-item">
+                  <div class="latest-news-item__thumb"><img src="assets/img/news-thumb-2.jpg" alt="news-thumb"></div>
+                  <div class="latest-news-item__info"><a class="latest-news-item__title"
+                      href="page-blog-article.html">Motorcycles Which Feels Just Right Like Ease.</a>
+                    <div class="latest-news-item__date">August 25, 2021</div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="latest-news-item">
+                  <div class="latest-news-item__thumb"><img src="assets/img/news-thumb-3.jpg" alt="news-thumb"></div>
+                  <div class="latest-news-item__info"><a class="latest-news-item__title"
+                      href="page-blog-article.html">Gaze Upon Unbelievably Exciting To Futuristic.</a>
+                    <div class="latest-news-item__date">August 25, 2021</div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div> --}}
+        <div>
+          <div class="page-footer__column page-footer__info">
+            <div class="uk-h4">Contact Us</div>
+            <ul class="info-list">
+
+
+              <li>
+                <div class="info-list-item">
+                  <div class="info-list-item__title">Call us on </div>
+                  <div class="info-list-item__value">03-7981 0331</div>
+                </div>
+              </li>
+
+
+              <li>
+                <div class="info-list-item">
+                  <div class="info-list-item__title">Operation Hours</div>
+                  <div class="info-list-item__value">Monday - Saturday: <br> 09:00 am to 07:00 pm</div>
+                </div>
+              </li>
+
+              <li>
+                <div class="info-list-item">
+                  <div class="info-list-item__title">Reach us by email</div>
+                  <div class="info-list-item__value">kuchaimotor@hotmail.my</div>
+                </div>
+              </li>
+
+
+              <li>
+                <div class="info-list-item">
+                  <div class="info-list-item__title">Showroom Address </div>
+                  <div class="info-list-item__value">kuchaimotor@hotmail.my</div>
+                </div>
+              </li>
+
+
+
+
+              {{-- <li>
+                <div class="info-list-item">
+                  <div class="info-list-item__title">Parts Shop Hours</div>
+                  <div class="info-list-item__value">Monday - Friday: <br> 09:00 am to 06:00 pm</div>
+                </div>
+              </li> --}}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-    <div class="uk-modal-full uk-modal" id="modal-search" data-uk-modal>
-      <div class="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle" data-uk-height-viewport><button class="uk-modal-close-full" type="button" data-uk-close></button>
-        <form class="uk-search uk-search-large"><input class="uk-search-input uk-text-center" type="search" placeholder="Search..." autofocus></form>
+  </div>
+  <div class="page-footer__bottom">
+    <div class="uk-container">
+      <div class="uk-text-center">Kuchai Motor Sdn Bhd . All rights reserved.</div>
+    </div>
+  </div>
+
+  <div id="offcanvas" data-uk-offcanvas="flip: true; overlay: true; mode: push">
+    <div class="uk-offcanvas-bar"><button class="uk-offcanvas-close" type="button" data-uk-close=""></button>
+      <div class="uk-margin">
+        <div class="logo"><a class="logo__link" href="index.html">
+            <div class="logo__icon">
+              <img src="{{asset('frontend/img/logo.png')}}" width="25%" alt="Kuchai Motor Sdn Bhd">
+            </div>
+            <div class="logo__text">Kuchai Motor</div>
+          </a></div>
       </div>
-    </div><!-- End Дополнительные блоки-->
-  </footer>
+      <div class="uk-margin">
+
+
+        <ul class="uk-nav uk-nav-default uk-nav-parent-icon" data-uk-nav>
+
+          <li><a href="{{route('home')}}">Home</a></li>
+
+          <li class="uk-parent"><a href="#">Motocycles</a>
+            <ul class="uk-nav-sub">
+              <li><a href="{{route('new.motors')}}">New Motorcycles</a></li>
+              <li><a href="{{route('used.motors')}}">Used Motorcycles</a></li>
+
+            </ul>
+          </li>
+
+          <li><a href="">Accessories</a></li>
+
+          <li><a href="{{route('used.motors')}}">Contact Us</a></li>
+
+
+          <li class="uk-parent"><a href="#">Languages</a>
+            <ul class="uk-nav-sub">
+              <li><a type="button" onclick="changeLanguage('en')">English </a></li>
+              <li><a type="button" onclick="changeLanguage('ms')">Bahasa Melayu</a></li>
+
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="uk-margin-auto-top">
+        <div class="support"><a class="support__link" href="tel:0379810331">
+            <div class="support__icon"><i class="fas fa-headset"></i></div>
+            <div class="support__desc">
+              <div class="support__label">Talk with our support</div>
+              <div class="support__phone">03-7981 0331</div>
+            </div>
+          </a></div>
+      </div>
+    </div>
+  </div>
+  <div class="uk-modal-full uk-modal" id="modal-search" data-uk-modal>
+    <div class="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle" data-uk-height-viewport><button
+        class="uk-modal-close-full" type="button" data-uk-close></button>
+      <form class="uk-search uk-search-large"><input class="uk-search-input uk-text-center" type="search"
+          placeholder="Search..." autofocus></form>
+    </div>
+  </div><!-- End Дополнительные блоки-->
+</footer>
